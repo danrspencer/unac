be987dd2a92f8d922a6e804a9f644ac43ddc340f
Dan Spencer <danrspen@gmail.com>
2018-05-02 10:36:08 +0100

    Removed the old stufF

--- script/com/Helper/IMoveMadeEvent.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="IEvent.ts" />
-
-interface IMoveMadeEvent extends IEvent {
-  add(listener: (id: string, winner: number, nextGridId: string) => void): void;
-  remove(listener: (id: string, winner: number, nextGridId: string) => void ): void;
-  trigger(id: string, winner: number, nextGridId: string): void;
-}
